c12feb9e5d730e7e8f8aeb002bc5739d0ba42872
Sem Glad <sem.glad@student.saimia.fi>
2017-09-13 12:35:39 +0300

    Added projects loops6 and loops7

---
+++ programming-basics/loops6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace loops6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Declare a variable to hold the value of the calculation
+            double a = 0;
+
+            // With numbers 1 to 10...
+            for (int i = 1; i <= 10; i++)
+            {
+
+                // ...calculate the square root of each value and round to two decimals
+                a = Math.Round(Math.Sqrt(i), 2);
+
+                // Print out the result
+                Console.WriteLine("Luvun {0} neliojuuri on {1}", i, a);
+
+            }
+
+            // Wait for any key to be pressed before exiting
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ programming-basics/loops7/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace loops7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Declare a variable to hold the value of the calculation
+            double a = 0;
+
+            // With numbers 1 to 10...
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int f = 1; f <= 10; f++)
+                {
+
+                    // ...calculate the product of i and f
+                    a = i * f;
+
+                    // Print out the result
+                    Console.WriteLine("{0} X {1} = {2}", i, f, a);
+
+                }
+
+                // Add a newline after each distinct value of i
+                Console.WriteLine("\n");
+
+            }
+
+            // Wait for any key to be pressed before exiting
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
