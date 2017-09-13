34c061452fdb1d3672afb6008d134ffa801a40a6
Sem Glad <sem.glad@student.saimia.fi>
2017-09-13 14:39:15 +0300

    Added functions-and-subprograms2 project

---
+++ programming-basics/functions-and-subprograms2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace functions_and_subprograms2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Prompt user for input
+            Console.Write("Anna jokin numero (erottele desimaalit pilkulla): ");
+
+            // Store user input to a variable
+            string userInput1 = Console.ReadLine().ToString();
+
+            // Parse user input into integer. If user 
+            double.TryParse(userInput1, out double inputnumber1);
+
+            // Prompt user for input
+            Console.Write("Anna jokin toinen numero: ");
+
+            // Store user input to a variable
+            string userInput2 = Console.ReadLine().ToString();
+
+            // Parse user input into integer. If user 
+            double.TryParse(userInput2, out double inputnumber2);
+
+            // Call function LesserNumber using user input as parameters
+            Console.WriteLine("Antamistasi numeroista {0} oli pienempi", LesserNumber(inputnumber1, inputnumber2));
+
+            // Wait for any key to be pressed before exiting
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+
+            double LesserNumber(double number1, double number2)
+            {
+                // Return the lesser number
+                return Math.Min(number1, number2);
+                
+            }
+        }
+    }
+}
