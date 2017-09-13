b4f074985010c89f3e3e1f018a812732a4ab15ab
Sem Glad <sem.glad@student.saimia.fi>
2017-09-13 12:55:07 +0300

    Added loops8 project

---
+++ programming-basics/loops8/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace loops8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Create a new instance rnd of object Random.
+            Random rnd = new Random();
+
+            // Loop through 20 lines
+            for (int i = 1; i <= 20; i++)
+            {
+
+                // Print out the row number
+                Console.Write("Row {0}: ", i);
+
+                // Loop through 5 lots per line
+                for (int f = 1; f <= 5; f++)
+                {
+
+                    // If it's not the final lot of the line...
+                    if (f != 5)
+                    {
+
+                        // Print out a random number trailed by a comma and a space
+                        Console.Write("{0}, ", rnd.Next(50));
+
+                    } else
+                    {
+
+                        // Print out a random number trailed by a newline
+                        Console.Write("{0}\n", rnd.Next(50));
+
+                    }
+
+
+                }
+            }
+
+            // Wait for any key to be pressed before exiting
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
