df1c91eab8f78a8e5e6152038c11f751b29cb743
Sem Glad <sem.glad@student.saimia.fi>
2017-09-13 12:06:23 +0300

    Added loops5 project and swapped the code between projects loops4 and loops5

--- programming-basics/loops5/Program.cs
+++ programming-basics/loops5/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace loops4
+namespace loops5
 {
     class Program
     {
@@ -13,20 +13,27 @@ namespace loops4
             // Store user input to a variable
             string userInput = Console.ReadLine().ToString();
 
-            // Declare variable to hold the result of each iteration and a flag to indicate whether the user input was a negative integer or not
-            int b = 0;
-            int negativeFlag = 0;
-
-            // Parse user input into integer
+            // Parse user input into integer. If user 
             int.TryParse(userInput, out int number);
 
+            // Declare a flag to indicate whether user input is negative or positive (negative=1, positive=0, value 0 by default)
+            int negativeFlag = 0;
+
+            // If user input is a negative integer...
             if (number < 0)
             {
 
+                // Raise the flag indicating that the user input is a negative integer
                 negativeFlag = 1;
+
+                // Convert the user input to its absolute value so it doesn't interfere with the loop
                 number = number * -1;
 
             }
+
+            // Declare variable to hold the result of each iteration
+            int b = 0;
+            int c = 0;
 
             // Loop through all the numbers between 1 and user input
             for (int a = 1; a <= number; a++)
@@ -41,22 +48,32 @@ namespace loops4
 
                 }
 
-                // Set b to the value of the sum of itself and the number of iterations so far
-                b = b + a;
+                // If a can be divided by two...
+                if (a % 2 == 0)
+                {
+                    // ...add number of iteration so far to the value of b
+                    b = b + a;
+
+                }
+                else
+                {
+
+                    // Else add number of iteration so far to the value of c
+                    c = c + a;
+
+                }
 
                 // If the user input was a negative integer...
                 if (negativeFlag == 1)
                 {
-
                     // Convert a back to its absolute value so it doesn't interfere with the loop
                     a = a * -1;
 
                 }
-
             }
 
             // Inform user of the result of the calculation
-            Console.WriteLine("{0}", b);
+            Console.WriteLine("Parillisten summa: {0}, parittomien summa: {1}", b, c);
 
             // Wait for any key to be pressed before exiting
             Console.WriteLine("Press any key to continue...");
