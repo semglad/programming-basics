c33fc85762bc1ecf17b3019bb23d8c7baa52fe41
Sem Glad <sem.glad@student.saimia.fi>
2017-09-14 22:40:05 +0300

    Added strings1 project

---
+++ programming-basics/strings1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace strings1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Prompt user for input
+            Console.WriteLine("Kirjoita jotain kivaa:");
+
+            // Store user input to a variable
+            string userInput = Console.ReadLine().ToString();
+
+            // Print out the length of the user input
+            Console.WriteLine("\nSyotteessa on {0} merkkia", userInput.Length);
+
+            // Wait for any key to be pressed before exiting
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+
+        }
+    }
+}
