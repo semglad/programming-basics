3140e14e54fc94e3026c09f2b11d2a4e818b4d79
Sem Glad <sem.glad@student.saimia.fi>
2017-09-15 02:13:14 +0300

    Added object-oriented-programming1 project

---
+++ programming-basics/object-oriented-programming1/Class1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Autotuning
+{
+
+    // Declare a new class
+    class Auto
+    {
+        // Declare class fields
+        public string merkki;
+        public double nopeus;
+
+        // Define the default constructor
+        public Auto()
+        {
+            
+            // Default values for the class fields
+            merkki = "Ford";
+            nopeus = 80;
+
+        }
+    
+        // Define a constructor with parameters
+        public Auto(string inputmerkki, double inputnopeus)
+        {
+
+            // Set class field values according to the values specified
+            merkki = inputmerkki;
+            nopeus = inputnopeus;
+
+        }
+
+        // Define a method to show the instance field values
+        public void NaytaTiedot()
+        {
+
+            Console.WriteLine("Merkki: {0}", merkki);
+            Console.WriteLine("Nopeus: {0}", nopeus);
+
+            // Wait for any key to be pressed before exiting
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+
+        }
+
+        // Define a method to increase the field "nopeus" value by the value specified
+        public void Kiihdyta(double muutos)
+        {
+
+            nopeus = nopeus + muutos;
+
+        }
+
+        // Define a method to decrease the field "nopeus" value by 10 percent
+        public void Jarruta()
+        {
+
+            nopeus = Math.Round(nopeus * 0.9);
+
+        }
+
+    }
+}
---
+++ programming-basics/object-oriented-programming1/Program.cs
@@ -0,0 +1,93 @@
+﻿using Autotuning;
+using System;
+
+namespace AutoTuning
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Create a new instance of the Auto class with the default constructor
+            Auto rotisko = new Auto();
+
+            // Show data for the instance
+            rotisko.NaytaTiedot();
+
+            // Clear the console and accelerate by 20 kph and show data
+            Console.Clear();
+            rotisko.Kiihdyta(20);
+            rotisko.NaytaTiedot();
+
+            // Clear the console and decelerate by 10 % and show data
+            Console.Clear();
+            rotisko.Jarruta();
+            rotisko.NaytaTiedot();
+
+            // Clear the screen and prompt user for input
+            Console.Clear();
+            Console.Write("Anna auton merkki: ");
+
+            // Store user input to a variable
+            string inputMerkki = Console.ReadLine().ToString();
+
+            // Prompt user for input
+            Console.Write("Anna auton nopeus kilometreina tunnissa: ");
+
+            // Store user input to a variable
+            string userInput = Console.ReadLine().ToString();
+
+            // Declare a variable for the speed value
+            double number = 0;
+
+            // If user input was not numerical...
+            while (!double.TryParse(userInput, out number))
+            {
+
+                // Give an error message
+                Console.Write("Virhe! Anna nopeus numerona: ");
+
+                // Prompt for a valid value
+                userInput = Console.ReadLine().ToString();
+
+            }
+
+            // Create a new instance of the Auto class with the user-defined parameters
+            Auto priima = new Auto(inputMerkki, number);
+
+            // Show data for the instance
+            Console.Clear();
+            priima.NaytaTiedot();
+
+            // Clear the console
+            Console.Clear();
+
+            // Prompt user for input
+            Console.Write("Paljonko nopeutta lisataan kilometreina tunnissa: ");
+
+            // Store user input to a variable
+            userInput = Console.ReadLine().ToString();
+
+            // If user input was not numerical...
+            while(!double.TryParse(userInput, out number))
+            {
+
+                // Give an error message
+                Console.Write("Virhe, anna numero! Paljonko nopeutta lisataan kilometreina tunnissa: ");
+
+                // Prompt for a valid value
+                userInput = Console.ReadLine().ToString();
+
+            }
+
+            // Clear the console and accelerate by the user-defined value and show data
+            Console.Clear();
+            priima.Kiihdyta(number);
+            priima.NaytaTiedot();
+
+            // Clear the console and decelerate by 10 % and show data
+            Console.Clear();
+            priima.Jarruta();
+            priima.NaytaTiedot();
+        }
+    }
+}
