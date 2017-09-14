5c86cc9f022f2c307170e484b5d29aebcf0b5bf2
Sem Glad <sem.glad@student.saimia.fi>
2017-09-14 23:02:05 +0300

    Added strings3 project

---
+++ programming-basics/strings2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace strings2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Prompt user for input
+            Console.WriteLine("Kirjoita jotain kivaa:");
+
+            // Store user input to a variable
+            string userInput = Console.ReadLine().ToString();
+
+            // Replace all "e" characters with "@" characters within the user input and print it out
+            Console.WriteLine(userInput.Replace("e", "@"));
+
+            // Wait for any key to be pressed before exiting
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ programming-basics/strings3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace strings3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Declare a variable to be used as a counter for "l" character occurrences in the user input
+            int counter = 0;
+
+            // Prompt user for input
+            Console.WriteLine("Kirjoita jotain kivaa:");
+
+            // Store user input to a variable
+            string userInput = Console.ReadLine().ToString();
+
+            // Loop through the user input character by character
+            for (int i = 0; i < userInput.Length; i++)
+            {
+
+                // If the current character equals "l"...
+                if (userInput[i] == System.Convert.ToChar("l"))
+                {
+
+                    // Increment the counter by one
+                    counter = counter + 1;
+
+                }
+
+            }
+
+            // Print out the number of "l" characters in the user input
+            Console.WriteLine(@"Syotteessa on {0} ""l"" merkkia", counter);
+
+            // Wait for any key to be pressed before exiting
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+
+        }
+    }
+}
