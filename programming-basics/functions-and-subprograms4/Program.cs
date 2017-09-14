ce3be12575b7f62cfe42242b4019f786af24e52a
Sem Glad <sem.glad@student.saimia.fi>
2017-09-14 22:32:59 +0300

    Finished commenting the code in project functions-and-subprograms4

--- programming-basics/functions-and-subprograms4/Program.cs
+++ programming-basics/functions-and-subprograms4/Program.cs
@@ -7,12 +7,15 @@ namespace functions_and_subprograms4
         static void Main(string[] args)
         {
 
+            // Declare variables for the series of integers, greatest integer and position of the greatest integer
             int a = 0;
             int positionOfGreatest = 0;
             string strNumbers = "";
             
+            // Call function GreatestOfTen with some dummy parameters
             a = GreatestOfTen(out strNumbers, out positionOfGreatest);
 
+            // Print out the integers input by the user, the greatest integer and the position of the greatest integer
             Console.WriteLine("\nSyottamasi lukusarja oli {0}", strNumbers);
             Console.WriteLine("\nSuurin syottamasi luku {0} oli {1}. luku sarjassa", a, positionOfGreatest);
 
@@ -20,15 +23,21 @@ namespace functions_and_subprograms4
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
 
+            // GreatestOfTen function is declared with two out parameters in order to return multiple values of multiple
+            // types
             int GreatestOfTen(out string strNumbers2, out int positionOfGreatest2)
             {
 
+                // Declare variables for the current integer, the greatest integer, integer series and the position of
+                // the greatest integer
                 int number = 0, b = 0;
                 strNumbers2 = "";
                 positionOfGreatest2 = 0;
 
+                // Inform the user of what they are expected to input
                 Console.WriteLine("Syota kymmenen lukua");
 
+                // Loop through 10 times
                 for (int i = 1; i <= 10; i++)
                 {
 
@@ -38,11 +47,11 @@ namespace functions_and_subprograms4
                     // Store user input to a variable
                     string userInput = Console.ReadLine().ToString();
 
-                    // Parse user input into integer
+                    // Parse user input into integer. If it's not a proper numerical value...
                     while ((!int.TryParse(userInput, out number)) | (number < 0))
                     {
 
-                        // Prompt user for input
+                        // ...display an error message and prompt again user for input
                         Console.WriteLine("Vaara syote, syota positiivinen luku!");
                         Console.Write("{0}. ", i);
                         
@@ -51,6 +60,8 @@ namespace functions_and_subprograms4
 
                     }
 
+                    // Append the user input into the string holding the input integers. Integers are preceded by
+                    // a comma and a space, except for the very first integer.
                     if (i > 1)
                     {
 
@@ -63,10 +74,12 @@ namespace functions_and_subprograms4
 
                     }
 
-
+                    // If the value of the current greatest integer is less than the one of the integer input by
+                    // the user...
                     if (b < number)
                     {
 
+                        // ...replace the value of the variable with the input and store the current integer position
                         b = number;
                         positionOfGreatest2 = i;
 
@@ -75,6 +88,7 @@ namespace functions_and_subprograms4
 
                 }
 
+                // Return the greatest integer
                 return b;
 
             }
